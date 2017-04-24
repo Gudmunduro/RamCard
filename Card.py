281c19c02386b424534e63355d826df92624abb8
Kristjan-O-Ragnarsson <kristjanra262@nemi.tskoli.is>
2017-04-24 16:09:37 +0000

    Added Card Sort_info and Reference

---
+++ Card.py
@@ -0,0 +1,20 @@
+"""
+Kristján
+Card class
+24/04/2017
+"""
+
+
+class Card:
+    """ Class for Icelandic Ram Info """
+    def __init__(self, _name, _home, _weight, _wool, _milk, _family, _hind_legs, _sexes, _meat, _ass):
+        self.name = _name
+        self.home = _home
+        self.weight = _weight
+        self.wool = _wool
+        self.milk = _milk
+        self.family = _family
+        self.hind_legs = _hind_legs
+        self.sexes = _sexes
+        self.meat = _meat
+        self.ass = _ass
