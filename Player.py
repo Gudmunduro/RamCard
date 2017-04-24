aa79348b6172487ac25bd1dd23f3d4a4cd42d37c
gudmunduroh <gudmundurha203@nemi.tskoli.is>
2017-04-24 16:30:05 +0000

    Bætt við player

---
+++ Player.py
@@ -0,0 +1,15 @@
+"""
+Guðmundur
+Player class
+24/4/2017
+"""
+
+
+class Player:
+
+    def __init__(self, name, cards):
+        self.name = name
+        self.cards = cards
+
+    def get_top_card(self):
+        pass
