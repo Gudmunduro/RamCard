f430eff4c251462cd69ba7e85cd2649ce3a1127d
gudmunduroh <gudmundurha203@nemi.tskoli.is>
2017-04-26 10:52:58 +0000

    Bætt við functions.py og main breytt

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ Guðmundur
 main class
 24/4/2017
 """
+from functions import *
 
 
 class Main:
 
     @staticmethod
     def main():
-        pass
+        Main.player_count = intinput("Sláðu inn hversu margir leikmenn eru", 1, 12)
+
+
 
 if __name__ == '__main__':
     Main.main()
