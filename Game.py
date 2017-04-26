bb0897cd91a0ca39fc6e7fd62617e732fc832c71
gudmunduroh <gudmundurha203@nemi.tskoli.is>
2017-04-26 11:07:22 +0000

    Breytingar

---
+++ Game.py
@@ -0,0 +1,14 @@
+"""
+Guðmundur
+game main class
+26/4/2017
+"""
+
+
+class Game:
+
+    def __init__(self, players):
+        self.players = players
+
+    def loop(self):
+        pass
--- Main.py
+++ Main.py
@@ -4,14 +4,27 @@ main class
 24/4/2017
 """
 from Functions import *
+from Game import Game
+from Player import Player
+from CardSplitter import CardSplitter
 
 
 class Main:
 
     @staticmethod
     def main():
-        Main.player_count = intinput("Sláðu inn hversu margir leikmenn eru", 1, 12)
+        player_count = intinput("Sláðu inn hversu margir leikmenn eru", 1, 12)
+        players = []
+        card_splitter = CardSplitter(player_count)
+        for i in range(player_count):
+            name = input("Sláðu inn nafn leikmanns " + str(i + 1))
+            cards = card_splitter.get_random_cards()
+            players.append(Player(name, cards))
+        game = Game(players)
+        while True:
+            game.loop()
 
 
 if __name__ == '__main__':
-    Main.main()
+    while True:
+        Main.main()
